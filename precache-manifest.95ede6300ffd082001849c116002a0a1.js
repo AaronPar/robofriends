4d9224b2dd50175ee4fc8bf296a50e98806ff776
AaronPar <20910737Par>
2019-06-26 15:33:45 +0200

    Updates

---
+++ precache-manifest.95ede6300ffd082001849c116002a0a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "0fd4a882ba5e4d359b3c",
+    "url": "/robofriends/static/js/main.0fd4a882.chunk.js"
+  },
+  {
+    "revision": "0aa5a029a61fe043e1fb",
+    "url": "/robofriends/static/js/2.0aa5a029.chunk.js"
+  },
+  {
+    "revision": "0fd4a882ba5e4d359b3c",
+    "url": "/robofriends/static/css/main.9b0fc5ce.chunk.css"
+  },
+  {
+    "revision": "0aa5a029a61fe043e1fb",
+    "url": "/robofriends/static/css/2.b6542a81.chunk.css"
+  },
+  {
+    "revision": "756502d4a3957de0acf7b42eec604a24",
+    "url": "/robofriends/index.html"
+  }
+];
